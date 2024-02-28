37ff9ba7d604b04a8ffc830ab7b9112063f48795
tabintel <favourekemini@yahoo.com>
2024-02-28 13:07:50 +0100

    updated API services

--- internal/services/rate/rate.go
+++ internal/services/rate/rate.go
@@ -2,9 +2,3 @@
 
 package rate
 
-// The rate.go file should not contain any redeclarations of ExchangeRateService and NewExchangeRateService.
-// It should only contain package-level variables and functions, if any.
-
-// If there are no package-level variables or functions in this file, it can be left empty.
-
-// Example of an empty file.
